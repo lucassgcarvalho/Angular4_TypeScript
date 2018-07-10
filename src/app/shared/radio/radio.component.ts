12ca186861007d076dfac54fefe18defcc9ce991
Lucas Carvalho <lucassgc_bpt@hotmail.com>
2018-07-10 15:56:27 -0300

    radio button, inputs forms

--- src/app/shared/radio/radio.component.ts
+++ src/app/shared/radio/radio.component.ts
@@ -1,15 +1,23 @@
 import { RadioOption } from './radio.module';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'mt-radio',
   templateUrl: './radio.component.html',
-  styleUrls: ['./radio.component.css']
+  styleUrls: ['./radio.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true
+    }
+  ]
 })
-export class RadioComponent implements OnInit {
-
+export class RadioComponent implements OnInit, ControlValueAccessor {
   @Input() options: RadioOption[];
   value: any;
+  onChange: any;
 
   constructor() {}
 
@@ -17,5 +25,25 @@ export class RadioComponent implements OnInit {
 
   setValues(value: any) {
     this.value = value;
+    this.onChange(this.value);
   }
+
+  /**
+   * Write a new value to the element.
+   */
+  writeValue(obj: any): void{
+    this.value = obj;
+  }
+  /**
+   * Set the function to be called when the control receives a change event.
+   */
+  registerOnChange(fn: any): void{
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+  }
+  setDisabledState?(isDisabled: boolean): void {
+  }
+
 }
